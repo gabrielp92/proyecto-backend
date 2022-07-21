109191717c17c4b459f8b0273383b8546af37e71
Gabriel Paez <gpaez1992@gmail.com>
2022-07-21 19:55:52 -0300

    desafío Websockets

---
+++ public/index.js
@@ -0,0 +1,53 @@
+const socket = io()
+loadFirstDataChat()
+const btnSend = document.getElementById('send')
+btnSend.onclick = e => {
+
+  let form = document.querySelector('.needs-validation')
+  let hayMail = true
+  if (!form.checkValidity()) {
+    hayMail = false
+    e.preventDefault()
+    e.stopPropagation()
+    form.classList.add('was-validated')
+  }
+  if(hayMail == true)
+  { 
+    let username = document.getElementById('username').value
+    let msn = document.getElementById('msn').value
+    socket.emit('chat-in', {msn, username})
+  }
+}
+
+socket.on('chat-out', data => {
+  addDataChatToDiv(data)
+})
+
+socket.on('producto-out', producto => {
+
+  let filaTabla = `
+    <tr class="border border-secondary">
+      <td class="p-4">${producto.title}</td>
+      <td class="p-4">${producto.price}</td>
+      <td class="p-4">
+        <img src="${producto.thumbnail}" alt="foto">
+      </td>
+    </tr>`
+   document.getElementById("body-tabla").innerHTML += filaTabla
+})
+
+function addDataChatToDiv(data){
+  document.getElementById("chat").innerHTML += `<br><b style="color:blue">${data.username}</b> <span style="color:brown">[${data.date} ${data.time}]</span><span style="color:green">: <i>${data.msn}</i></span>`
+}
+
+function loadDataChatToDiv(data){
+  data.forEach(d => addDataChatToDiv(d))
+}
+
+function loadFirstDataChat(){
+
+  fetch('/dataChat')
+    .then(data => data.json())
+    .then(d => loadDataChatToDiv(d.data))
+    .catch(e => alert(e))
+}
--- server.js
+++ server.js
@@ -1,39 +1,68 @@
 const Contenedor = require('./Contenedor')
-const contenedor = new Contenedor('productos.json');
+const contenedorProductos = new Contenedor('productos.json');
 (async function(){
-    await contenedor.init()
+    await contenedorProductos.init()
+})();
+const contenedorChat = new Contenedor('chat.json');
+(async function(){
+    await contenedorChat.init()
 })();
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 const app = express()
-const PORT = process.env.PORT || 8080
+const server = http.createServer(app)
+const io = new Server(server) //servidor de web socket
+
 app.set('views', './views')
 app.set('view engine', 'ejs')
+app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use('/uploads', express.static('uploads'))
+app.use('/public', express.static(__dirname + '/public'))
 app.use((err,req,res,next) => {
     console.error(err)
     res.status(500).send('Hubo algún error')
 })
-/***************************************/
 
-app.get('/', (req,res) => res.sendFile(__dirname + '/views/form.html'))
+app.get('/', (req,res) => {
+    const tituloTabla = 'Lista de Productos'
+    const productos = contenedorProductos.getAll()
+    res.render('index', { productos , tituloTabla })
+})
 
-app.get('/productos', (req,res) => {
-
-    const mensaje = 'Vista de Productos'
-    const productos = contenedor.getAll()
-    res.render('productos', { productos , mensaje })
+app.get('/dataChat', (req,res) => {
+    const data = contenedorChat.getAll()
+    res.json({data})
 })
 
 app.post('/productos', (req, res) => {   
-    contenedor.save(req.body)
-        .then( () => res.redirect('/'))
+    contenedorProductos.save(req.body)
+        .then( () => {
+            io.sockets.emit('producto-out',req.body)
+            res.redirect('/')
+        })
         .catch( () => res.send('Error to save'))
 })
 
-const server = app.listen(PORT, () => {
-    console.log(`Server listening [${PORT}]...`)
+io.on('connection', (socket) => {
+    
+    socket.on('chat-in', data => {
+        const date = new Date().toLocaleDateString()
+        const time = new Date().toLocaleTimeString()
+        const dataOut = {
+            msn : data.msn,
+            username : data.username,
+            date,
+            time
+        };
+        (async function(){
+            await contenedorChat.save(dataOut)
+        })();
+        io.sockets.emit('chat-out', dataOut)
+    })
+  
 })
 
-server.on('error', e => console.log('error en el server. ',e))
+const PORT = process.env.PORT || 8080
+server.listen(8080, () => console.log('server running.....'))
