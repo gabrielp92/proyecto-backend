c532d66ecbdcb18a068a5a8f209229a33c92000e
Gabriel Paez <gpaez1992@gmail.com>
2022-10-21 20:27:20 -0300

    se agrega la ruta /heroku

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get('/', (req,res) => {
     if(req.isAuthenticated())
         res.sendFile(__dirname + '/public/index.html')
     else
-        res.redirect('/login')
+    res.sendFile(__dirname + '/public/login.html')
 })
 
 app.get('/heroku', (req,res) => {
